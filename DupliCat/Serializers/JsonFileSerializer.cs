fb9118a1e41e8f134792f93c7a8339293feb2689
Christian Ramelow <info@codekandis.net>
2020-09-03 17:11:10 +0200

    Adds the JSON File Serializer.

---
+++ DupliCat/Serializers/JsonFileSerializer.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace DupliCat.Serializers
+{
+	internal class JsonFileSerializer<TData>:
+		IJsonFileSerializer<TData>
+	{
+		public virtual void Serialize( TData data, string path )
+		{
+			using ( FileStream stream = new FileStream( path, FileMode.Create ) )
+			{
+				using ( StreamWriter streamWriter = new StreamWriter( stream ) )
+				{
+					streamWriter.Write( JsonConvert.SerializeObject( data ) );
+				}
+			}
+		}
+
+		public virtual TData Deserialize( string path )
+		{
+			using ( FileStream stream = new FileStream( path, FileMode.Open ) )
+			{
+				using ( StreamReader streamReader = new StreamReader( stream ) )
+				{
+					TData deserializedData = JsonConvert.DeserializeObject<TData>( streamReader.ReadToEnd() );
+					return deserializedData;
+				}
+			}
+		}
+	}
+}
