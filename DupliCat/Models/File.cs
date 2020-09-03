50d676f2be3b304f276d146a6941cd112e5c5347
Christian Ramelow <info@codekandis.net>
2020-09-03 04:26:41 +0200

    Adds the necessary models of MD5 checksums and their associated files.

---
+++ DupliCat/Models/File.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using SharpKandis.ComponentModel;
+
+namespace DupliCat.Models
+{
+	[JsonObject( MemberSerialization.OptIn )]
+	internal class File:
+		NotifyPropertyAbstract,
+		IFile
+	{
+		[JsonProperty( PropertyName = "path" )]
+		private string path;
+
+		public virtual string Path
+		{
+			get
+			{
+				return this.path;
+			}
+			private set
+			{
+				this.PropertyChangingRaise();
+				this.path = value;
+				this.PropertyChangedRaise();
+			}
+		}
+
+		[JsonProperty( PropertyName = "flagDeletion" )]
+		private bool flagDeletion = false;
+
+		public virtual bool FlagDeletion
+		{
+			get
+			{
+				return this.flagDeletion;
+			}
+			set
+			{
+				this.PropertyChangingRaise();
+				this.flagDeletion = value;
+				this.PropertyChangedRaise();
+			}
+		}
+
+		public File( string path )
+		{
+			this.Path = path;
+		}
+	}
+}
---
+++ DupliCat/Models/Md5List.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using DupliCat.Models.Converters;
+using Newtonsoft.Json;
+
+namespace DupliCat.Models
+{
+	[JsonArray( ItemConverterType = typeof( ListItemConverter<Md5> ) )]
+	internal class Md5List:
+		List<IMd5>
+	{
+	}
+}
