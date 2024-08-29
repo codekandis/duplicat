1d6328c02f0cca0b727fda88009372a161c42ecd
Christian Ramelow <info@codekandis.net>
2024-08-29 21:04:27 +0200

    Adds inline documentation.

--- DupliCat/Forms/Main.cs
+++ DupliCat/Forms/Main.cs
@@ -12,23 +12,48 @@ using File = System.IO.File;
 
 namespace CodeKandis.DupliCat.Forms
 {
-	internal partial class Main: Form
+	/// <summary>
+	/// Represents the main form of the application.
+	/// </summary>
+	internal partial class Main:
+		Form
 	{
+		/// <summary>
+		/// Represents the path of the listing.
+		/// </summary>
 		private const string ListingPath = @"D:\development\csharp\codekandis\projects\DupliCat\DupliCat\listing.json";
 
+		/// <summary>
+		/// Stores the bindable data source of MD5 sets.
+		/// </summary>
 		private BindableDataSource<Md5SetInterface> md5Sets;
 
+		/// <summary>
+		/// Stores the MD5 sets.
+		/// </summary>
 		private Md5SetListInterface md5SetList = new Md5SetList();
 
+		/// <summary>
+		/// Stores the bindable data source of files.
+		/// </summary>
 		private BindableDataSource<FileInterface> files;
 
+		/// <summary>
+		/// Stores the files.
+		/// </summary>
 		private readonly FileListInterface fileList = new FileList();
 
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
 		public Main()
 		{
 			this.InitializeComponent();
 		}
 
+		/// <summary>
+		/// Initializes the form.
+		/// </summary>
 		private void Initialize()
 		{
 			this.md5Sets = new BindableDataSource<Md5SetInterface>();
@@ -43,6 +68,10 @@ namespace CodeKandis.DupliCat.Forms
 			this.LoadListing();
 		}
 
+		/// <summary>
+		/// Logs a message.
+		/// </summary>
+		/// <param name="message">The message to log.</param>
 		private void Log( string message )
 		{
 			this.Invoke(
@@ -54,6 +83,9 @@ namespace CodeKandis.DupliCat.Forms
 			);
 		}
 
+		/// <summary>
+		/// Loads the listing.
+		/// </summary>
 		private void LoadListing()
 		{
 			this.md5SetList = new Md5SetListJsonFileDeserializer( Main.ListingPath )
@@ -66,17 +98,26 @@ namespace CodeKandis.DupliCat.Forms
 			this.lblTotal.Text = this.md5Sets.Count.ToString();
 		}
 
+		/// <summary>
+		/// Saves the listing.
+		/// </summary>
 		private void SaveListing()
 		{
 			new Md5SetListJsonFileSerializer( Main.ListingPath )
 				.Serialize( this.md5SetList );
 		}
 
+		/// <summary>
+		/// Toggles the log visibility.
+		/// </summary>
 		private void ToggleLog()
 		{
 			this.pnlLog.Visible = !this.pnlLog.Visible;
 		}
 
+		/// <summary>
+		/// Scans the directory.
+		/// </summary>
 		private void Scan()
 		{
 			Thread thread = new Thread(
@@ -174,6 +215,9 @@ namespace CodeKandis.DupliCat.Forms
 			thread.Start();
 		}
 
+		/// <summary>
+		/// Purges the listing from single file entries.
+		/// </summary>
 		private void Purge()
 		{
 			for ( int n = this.md5SetList.Count - 1; n >= 0; n-- )
@@ -188,6 +232,9 @@ namespace CodeKandis.DupliCat.Forms
 			this.lblTotal.Text = this.md5Sets.Count.ToString();
 		}
 
+		/// <summary>
+		/// Marks all duplicates for deletion.
+		/// </summary>
 		private void Mark()
 		{
 			foreach ( Md5SetInterface md5Set in this.md5SetList )
@@ -201,6 +248,9 @@ namespace CodeKandis.DupliCat.Forms
 			this.md5Sets.DataSource.ResetBindings();
 		}
 
+		/// <summary>
+		/// Deletes all marked duplicates.
+		/// </summary>
 		private void Delete()
 		{
 			foreach ( Md5SetInterface md5Set in this.md5SetList )
@@ -230,62 +280,120 @@ namespace CodeKandis.DupliCat.Forms
 			this.md5Sets.DataSource.ResetBindings();
 		}
 
+		/// <summary>
+		/// Removes remaining empty directories.
+		/// </summary>
 		private void RemoveEmptyDirs()
 		{
 			new RecursivelyEmptyDirectoryRemover( this.tbxPath.Text )
 				.Remove();
 		}
 
+		/// <summary>
+		/// Represents the event handler if the form has been loaded.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void this_Load( object sender, EventArgs eventArguments )
 		{
 			this.Initialize();
 		}
 
+		/// <summary>
+		/// Represents the event handler if the current item of the bound data source of MD5 sets has been changed.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void md5Sets_CurrentChanged( object sender, EventArgs eventArguments )
 		{
 			this.files.RefreshWith( this.md5Sets.Current.Files );
 		}
 
+		/// <summary>
+		/// Represents the event handler if the `Scan` button has been clicked.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void btnScan_Click( object sender, EventArgs eventArguments )
 		{
 			this.Scan();
 		}
 
+		/// <summary>
+		/// Represents the event handler if the `Purge` button has been clicked.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void btnPurge_Click( object sender, EventArgs eventArguments )
 		{
 			this.Purge();
 		}
 
+		/// <summary>
+		/// Represents the event handler if the `Mark` button has been clicked.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void btnMark_Click( object sender, EventArgs eventArguments )
 		{
 			this.Mark();
 		}
 
+		/// <summary>
+		/// Represents the event handler if the `Delete` button has been clicked.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void btnDelete_Click( object sender, EventArgs eventArguments )
 		{
 			this.Delete();
 		}
 
-		private void btnEmptyDirs_Click( object sender, EventArgs e )
+		/// <summary>
+		/// Represents the event handler if the `Empty Dirs` button has been clicked.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
+		private void btnEmptyDirs_Click( object sender, EventArgs eventArguments )
 		{
 			this.RemoveEmptyDirs();
 		}
 
+		/// <summary>
+		/// Represents the event handler if the `Load` button has been clicked.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void btnLoad_Click( object sender, EventArgs eventArguments )
 		{
 			this.LoadListing();
 		}
 
+		/// <summary>
+		/// Represents the event handler if the `Save` button has been clicked.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void btnSave_Click( object sender, EventArgs eventArguments )
 		{
 			this.SaveListing();
 		}
 
+		/// <summary>
+		/// Represents the event handler if the `Log` button has been clicked.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void btnLog_Click( object sender, EventArgs eventArguments )
 		{
 			this.ToggleLog();
 		}
 
+		/// <summary>
+		/// Represents the event handler if the text box of the path has been double clicked.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void tbxPath_DoubleClick( object sender, EventArgs eventArguments )
 		{
 			if ( DialogResult.OK == this.fbdlgPath.ShowDialog( this.Parent ) )
@@ -294,6 +402,11 @@ namespace CodeKandis.DupliCat.Forms
 			}
 		}
 
+		/// <summary>
+		/// Represents the event handler if the current item of the list box of MD5 sets will be formatted.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void lbxMd5Sets_Format( object sender, ListControlConvertEventArgs eventArguments )
 		{
 			Md5SetInterface md5Set = ( Md5SetInterface ) eventArguments.ListItem;
@@ -312,6 +425,11 @@ namespace CodeKandis.DupliCat.Forms
 			eventArguments.Value = $"{index} :: {md5Set.Checksum}";
 		}
 
+		/// <summary>
+		/// Represents the event handler if the current item of the list box of files sets will be formatted.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void lbxFiles_Format( object sender, ListControlConvertEventArgs eventArguments )
 		{
 			FileInterface file = ( FileInterface ) eventArguments.ListItem;
@@ -321,6 +439,11 @@ namespace CodeKandis.DupliCat.Forms
 			eventArguments.Value = $"{prefix} {file.Path}";
 		}
 
+		/// <summary>
+		/// Represents the event handler if the list box of files has been double clicked.
+		/// </summary>
+		/// <param name="sender">The object which raised the event.</param>
+		/// <param name="eventArguments">The arguments of the event.</param>
 		private void lbxFiles_MouseDoubleClick( object sender, MouseEventArgs eventArguments )
 		{
 			int index = this.lbxFiles.IndexFromPoint( eventArguments.Location );
