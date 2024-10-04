248ffd696da7eb3f9756d724576b4ccb59adb094
Christian Ramelow <info@codekandis.net>
2024-10-05 01:28:52 +0200

    Adds enhanced exception handling.

--- DupliCat/Data/FileList.cs
+++ DupliCat/Data/FileList.cs
@@ -10,4 +10,21 @@ namespace CodeKandis.DupliCat.Data;
 [ JsonArray( ItemConverterType = typeof( JsonFileListItemConverter ) ) ]
 internal class FileList:
 	List<FileInterface>,
-	FileListInterface;
+	FileListInterface
+{
+	/// <summary>
+	/// Constructor method.
+	/// </summary>
+	public FileList()
+	{
+	}
+
+	/// <summary>
+	/// Constructor method.
+	/// </summary>
+	/// <param name="initialFiles">The initial files of the list.</param>
+	public FileList( IEnumerable<FileInterface> initialFiles )
+		: base( initialFiles )
+	{
+	}
+}
