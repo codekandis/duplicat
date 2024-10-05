79aba59c8820638ff7813e0e008f8c14fe726315
Christian Ramelow <info@codekandis.net>
2024-10-05 20:43:58 +0200

    Adds a file flagger.

---
+++ DupliCat/Io/FileFlagger.cs
@@ -0,0 +1,14 @@
+using CodeKandis.DupliCat.Data;
+
+namespace CodeKandis.DupliCat.Io;
+
+/// <summary>Represents a file flagger.</summary>
+internal class FileFlagger:
+	FileFlaggerInterface
+{
+	/// <inheritdoc />
+	public virtual void Flag( FileInterface file )
+	{
+		file.FlagDeletion = true;
+	}
+}
