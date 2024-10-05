770392619443c004e4bb623db8c47e95eaa8c133
Christian Ramelow <info@codekandis.net>
2024-10-05 20:48:46 +0200

    Updates the file deleter.

--- DupliCat/Io/FileDeleterInterface.cs
+++ DupliCat/Io/FileDeleterInterface.cs
@@ -1,9 +1,11 @@
+using CodeKandis.DupliCat.Data;
+
 namespace CodeKandis.DupliCat.Io;
 
 /// <summary>Represents the interface of any file deleter.</summary>
 internal interface FileDeleterInterface
 {
 	/// <summary>Deletes a file.</summary>
-	/// <param name="path">The path of the file.</param>
-	void Delete( string path );
+	/// <param name="file">The path of the file.</param>
+	void Delete( FileInterface file );
 }
