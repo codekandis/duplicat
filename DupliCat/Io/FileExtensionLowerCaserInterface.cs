9bb982db57f178409b6a975ca4f24379acd2e03f
Christian Ramelow <info@codekandis.net>
2024-10-06 00:37:26 +0200

    Updates file extension lower caser.

--- DupliCat/Io/FileExtensionLowerCaserInterface.cs
+++ DupliCat/Io/FileExtensionLowerCaserInterface.cs
@@ -1,10 +1,11 @@
+using CodeKandis.DupliCat.Data;
+
 namespace CodeKandis.DupliCat.Io;
 
 /// <summary>Represents the interface of any file extension lower caser.</summary>
 internal interface FileExtensionLowerCaserInterface
 {
 	/// <summary>Lower cases the file extension.</summary>
-	/// <param name="path">The path of the file to lower case its extension.</param>
-	/// <returns>The new path of the file.</returns>
-	string LowerCase( string path );
+	/// <param name="file">The file to lower case its extension.</param>
+	void LowerCase( FileInterface file );
 }
